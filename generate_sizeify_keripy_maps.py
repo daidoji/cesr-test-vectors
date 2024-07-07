2dc71fb7ec28d778c9241cab0f00f7f298f7964f
Charles Lanahan <charles.lanahan@gmail.com>
2024-07-07 12:28:43 +0900

    checkpoint commit.  Tests fail here though

--- generate_sizeify_keripy_maps.py
+++ generate_sizeify_keripy_maps.py
@@ -8,6 +8,7 @@ import pytest
 import keri.core.coring as coring
 from keri.kering import Version, Versionage, versify, deversify
 
+I = 0
 
 def init():
     os.makedirs("example_payloads/keripy_tests/", exist_ok=True)
@@ -27,6 +28,7 @@ def monkey_patch_sizeify():
 
     # If this signature changes this will probably break in weird ways
     def new_sizeify(ked, kind=None, version=Version):
+        global I
         old_ked = copy.deepcopy(ked)
         # old_kind = copy.deepcopy(kind)
         # old_version = copy.deepcopy(version)
@@ -36,23 +38,47 @@ def monkey_patch_sizeify():
         
         for v, k in itertools.product((v1_tuple, v2_tuple), 
                                       ('JSON', 'MGPK', 'CBOR')):
-            try:
-                working_ked = copy.deepcopy(ked)
-                proto, _vrsn, _kind, _size = deversify(working_ked['v'])
-                working_ked['v'] = versify(proto, v, k, 0)
-                #print(working_ked['v'])
-            except Exception as e:
-                import pdb; pdb.set_trace()
-            cesr_message, _proto, _kind, _ked, _vrsn = old_sizeify(ked=working_ked, kind=k, version=v)
-            hashed_filename = hashlib.md5(cesr_message).hexdigest()
-            with open(old_directory + f"/example_payloads/keripy_tests/v{v.major}/{k}/{hashed_filename}", "wb") as fyle:
-                fyle.write(cesr_message)
+            working_ked = copy.deepcopy(ked)
+            proto, _vrsn, _kind, _size = deversify(working_ked['v'])
+            working_ked['v'] = versify(proto, v, k, 0)
+
+            if working_ked.get('dt'):
+                working_ked['dt'] = '2023-06-24T12:34:56Z'
+
+            # working_ked = replace_n_values_in_dict(working_ked)
+
+            # cesr_message, _proto, _kind, _ked, _vrsn = old_sizeify(ked=working_ked, kind=k, version=v)
+
+            # hashed_filename = hashlib.md5(cesr_message).hexdigest()
+            #with open(old_directory + f"/example_payloads/keripy_tests/v{v.major}/{k}/{hashed_filename}", "wb") as fyle:
+            # with open(old_directory + f"/example_payloads/keripy_tests/v{v.major}/{k}/{I}", "wb") as fyle:
+             #    fyle.write(cesr_message)
+
+        I += 1
 
         # We run whatever was passed in originally
         return old_sizeify(ked=old_ked, kind=kind, version=version)
 
     # Monkeypatch
     coring.sizeify = new_sizeify
+
+
+def replace_n_values_in_dict(ked):
+    new_dict = {}
+    for k, v in ked.items():
+        match k:
+            case "n": # Next pub_key commitments
+                if isinstance(v, list):
+                    new_val = [("X" * len(pub_key)) for pub_key in v]
+                elif isinstance(v, str):
+                    new_val = "X" * len(v)
+                else:
+                    new_val = v
+                new_dict[k] = new_val
+            case _:
+                new_dict[k] = v
+    return new_dict
+
 
 def cd_to_keripy_repo_run_pytest():
     keri_repo = os.getenv("KERI_REPO")
